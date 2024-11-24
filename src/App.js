ab8a1aae67775cfb7c48e8e15f359791d7ac8b78
Dave ZAGABE <davezag12@gmail.com>
2024-11-24 22:45:52 +0300

    Add logic to handle theme swtich

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import NavBar from "./components/navbar/Navbar";
@@ -10,8 +11,10 @@ import Properties from "./pages/Properties";
 import Subscribe from "./pages/Subscribe";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div className="app" data-theme="light">
+    <div className="app" data-theme={isDark ? "light" : "dark"}>
       <BrowserRouter>
         <NavBar />
         <Loading />
